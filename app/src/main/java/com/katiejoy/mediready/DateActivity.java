42fcebb385445a433eb06df5acf1125cb87407b7
KatieJoy <katiejoysweaney@gmail.com>
2017-04-09 16:03:42 -0500

    Passed the selected date to a new activity. Now it opens up a new page that says the date you chose from the calendar. Changed calendar accent color to match the logo.

--- app/src/main/java/com/katiejoy/mediready/DateActivity.java
+++ app/src/main/java/com/katiejoy/mediready/DateActivity.java
@@ -5,26 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DateActivity extends AppCompatActivity {
-
-    Button goHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date);
 
-        goHomeButton = (Button) findViewById(R.id.goHomeButton);
-        goHomeButton.setOnClickListener(new View.OnClickListener() {
+        Button button;
+        button = (Button) findViewById(R.id.goHomeButton);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DateActivity.this, MainActivity.class);
-
-                //intent.putExtra();
-
-                startActivity(intent);
+                finish();
             }
         });
+
+
+        Intent intent = getIntent();
+
+        int month = intent.getIntExtra("month", 0);
+        int day = intent.getIntExtra("day", 0);
+        int year = intent.getIntExtra("year", 0);
+
+        TextView dateText = (TextView)findViewById(R.id.dateText);
+        dateText.setText("Date Chosen: "+ (month+1) + "/" + day + "/" + year);
     }
 }
+
--- app/src/main/java/com/katiejoy/mediready/MainActivity.java
+++ app/src/main/java/com/katiejoy/mediready/MainActivity.java
@@ -1,19 +1,32 @@
 package com.katiejoy.mediready;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void setDate(View view) {
-        PickerDialogs pickerDiaglogs = new PickerDialogs();
-        pickerDiaglogs.show(getSupportFragmentManager(), "date_picker");
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerActivity();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
 }
