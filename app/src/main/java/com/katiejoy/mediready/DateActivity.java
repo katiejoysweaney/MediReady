88831325fac6523c4c0e5773e2c21575d0004209
KatieJoy <katiejoysweaney@gmail.com>
2017-04-13 09:27:14 -0500

    Removed big blue bar at the top. Made background the same grey for the date activity. Made fontsize bigger and the same dark red for date activity. Fixed strings.xml.

--- app/src/main/java/com/katiejoy/mediready/DateActivity.java
+++ app/src/main/java/com/katiejoy/mediready/DateActivity.java
@@ -1,13 +1,17 @@
 package com.katiejoy.mediready;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class DateActivity extends AppCompatActivity {
+
+    //SQLiteDatabase eventsdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,17 @@ public class DateActivity extends AppCompatActivity {
         int year = intent.getIntExtra("year", 0);
 
         TextView dateText = (TextView)findViewById(R.id.dateText);
-        dateText.setText("Date Chosen: "+ (month+1) + "/" + day + "/" + year);
+        dateText.setText("Date: "+ (month+1) + "/" + day + "/" + year);
+
+        /*
+        try {
+            eventsdb = this.openOrCreateDatabase("EVENTSDB", MODE_PRIVATE, null);
+            eventsdb.execSQL("CREATE TABLE IF NOT EXISTS EVENTS (key INT, month INT, day INT, year INT, title VARCHAR)");
+
+        } catch (Exception e) {
+            Log.i("error", e);
+        }
+        */
     }
 }
 
